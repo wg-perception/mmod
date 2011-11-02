54665d05ccceaad381c458cac19d5f5a6500cdeb
Vincent Rabaud <vrabaud@willowgarage.com>
2011-11-01 17:58:46 -0700

    - conform to the new loader

--- cells/MModModelInserter.cpp
+++ cells/MModModelInserter.cpp
@@ -3,8 +3,6 @@
 
 #include <boost/foreach.hpp>
 #include <boost/format.hpp>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>
 #include <boost/python.hpp>
 #include <boost/python/stl_iterator.hpp>
 
@@ -13,14 +11,16 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#include "object_recognition/common/json_spirit/json_spirit.h"
+#include "object_recognition/common/types.h"
 #include "object_recognition/db/db.h"
 #include "object_recognition/db/opencv.h"
 
 #include "mmod_features.h"
 #include "mmod_objects.h"
 
-using object_recognition::db_future::CollectionName;
-using object_recognition::db_future::DocumentId;
+using object_recognition::db::CollectionName;
+using object_recognition::db::DocumentId;
 
 namespace mmod
 {
@@ -31,28 +31,29 @@ namespace mmod
     static void
     declare_params(ecto::tendrils& params)
     {
-      params.declare<std::string> ("collection_models",
-                                   "std::string The collection in which to store the models on the db", "models").required(
-                                                                                                                           true);
-      params.declare<object_recognition::db_future::ObjectDbParameters> ("db_params", "The DB parameters").required(true);
-      params.declare<std::string> ("object_id", "The object id, to associate this frame with.").required(true);
-      params.declare<std::string> ("model_json_params", "The parameters used for the model, as JSON.").required(true);
+      params.declare < std::string
+      > ("collection_models", "std::string The collection in which to store the models on the db", "models").required(
+          true);
+      params.declare < object_recognition::db::ObjectDbParameters > ("db_params", "The DB parameters").required(true);
+      params.declare < std::string > ("object_id", "The object id, to associate this frame with.").required(true);
+      params.declare < std::string
+      > ("model_json_params", "The parameters used for the model, as JSON.").required(true);
     }
 
     static void
     declare_io(const ecto::tendrils& params, ecto::tendrils& inputs, ecto::tendrils& outputs)
     {
-      inputs.declare<mmod_objects> ("objects", "The objects.");
-      inputs.declare<mmod_filters> ("filters", "The filters.");
+      inputs.declare < mmod_objects > ("objects", "The objects.");
+      inputs.declare < mmod_filters > ("filters", "The filters.");
     }
 
     void
     configure(const ecto::tendrils& params, const ecto::tendrils& inputs, const ecto::tendrils& outputs)
     {
       object_id_ = params["object_id"];
-      db_.set_params(params.get<object_recognition::db_future::ObjectDbParameters> ("db_params"));
-      collection_models_ = params.get<std::string> ("collection_models");
-      params_ = params.get<std::string> ("model_json_params");
+      db_.set_params(params.get < object_recognition::db::ObjectDbParameters > ("db_params"));
+      collection_models_ = params.get < std::string > ("collection_models");
+      params_ = params.get < std::string > ("model_json_params");
 
       objects_ = inputs["objects"];
       filters_ = inputs["filters"];
@@ -61,7 +62,7 @@ namespace mmod
     int
     process(const ecto::tendrils& inputs, const ecto::tendrils& outputs)
     {
-      object_recognition::db_future::Document doc(db_, collection_models_);
+      object_recognition::db::Document doc(db_, collection_models_);
 
       {
         std::stringstream objects_stream;
@@ -78,12 +79,15 @@ namespace mmod
       doc.set_value("object_id", *object_id_);
 
       // Convert the parameters to a property tree and insert them
-      boost::property_tree::ptree params;
-      std::stringstream ssparams;
-      ssparams << params_;
-      boost::property_tree::read_json(ssparams, params);
-      if (params.find("type") != params.not_found())
-        params.erase("type");
+      json_spirit::mObject params;
+      {
+        std::stringstream ssparams;
+        ssparams << params_;
+        json_spirit::mValue value;
+        json_spirit::read(ssparams, value);
+        params = value.get_obj();
+      }
+      params.erase("type");
       doc.set_values("parameters", params);
 
       doc.set_value("Type", "Model");
@@ -94,7 +98,7 @@ namespace mmod
       return ecto::OK;
     }
   private:
-    object_recognition::db_future::ObjectDb db_;
+    object_recognition::db::ObjectDb db_;
     ecto::spore<DocumentId> object_id_;
     CollectionName collection_models_;
     /** The JSON parameters used to compute the model */
--- cells/scripts/testing.py
+++ cells/scripts/testing.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 import ecto
+import ecto_ros
 import sys
 import couchdb
+from argparse import ArgumentParser
 from ecto_opencv.highgui import imshow
 from ecto_object_recognition import capture
-from object_recognition import models, dbtools
+from object_recognition import models
 import mmod
 from ecto_object_recognition.object_recognition_db import ObjectDbParameters
 from object_recognition.common.io.source import Source
+from object_recognition.common.io.sink import Sink
+from object_recognition.common.io.source import Source
+from object_recognition.common.utils.training_detection_args import read_arguments
+from object_recognition.common.utils import json_helper
 
 # FROM AN OLDER FILE ...
 import argparse
@@ -22,67 +28,42 @@ import subprocess
 
 from mmod import MModTester
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Test mmod features on a sequence of inputs.')
-    parser.add_argument('-i', '--training', metavar='TRAINING_FILE', dest='training', type=str, default='',
-                       help='The training file')
-    parser.add_argument('--use_kinect', dest='use_kinect', action='store_true',
-                        default=False, help='Use a highres kinect')
-    parser.add_argument('--use_db', dest='use_db', action='store_true',
-                        default=True, help='Use the db to test')
-    dbtools.add_db_options(parser)
-    args = parser.parse_args()
-    if args.training == '':
-        parser.print_usage()
-        print 'You must supply a training file.'
-        sys.exit(1)
-    return args
-
-def test_mmod(args):
+if "__main__" == __name__:
     '''
     Run mmod testing
     '''
     plasm = ecto.Plasm()
     parser = ArgumentParser()
+    argv = sys.argv
 
     # add arguments for the source and sink
     Sink.add_arguments(parser)
 
-    params, args, pipeline_params, do_display, db_params, db = read_arguments(parser,argv)
+    params, args, pipeline_params, do_display, db_params, db = read_arguments(parser, argv[1:])
 
     model_ids = []
-    object_ids = []
+    object_ids = set()
     for object_id in params['object_ids']:
         for model_id in models.find_model_for_object(db, object_id, 'TOD'):
             model_ids.append(str(model_id))
-            object_ids.append(object_id)
-    params['object_ids'] = object_ids
+            object_ids.add(object_id)
+    model_documents = DbDocuments(db_params, model_ids)
 
     # TODO handle this properly...
-    ecto_ros.init(argv, "tod_detection", False)#not anonymous.
+    ecto_ros.init(argv, "mmod_testing", False)#not anonymous.
 
     source = Source.parse_arguments(params['source'])
 
 
-    sink = Sink.parse_arguments(args, db, db_params, params['object_ids'])
+    sink = Sink.parse_arguments(args, db, db_params, object_ids)
 
-    
-    
-    
     #hook up the tester
-    loader = mmod.TemplateLoader(collection_models=db_params.collection,
-                                           db_params=db_params,
-                                           object_ids=object_ids, model_ids=model_ids,
-                                           feature_descriptor_params=json_helper.dict_to_cpp_json_str(pipeline_param['feature_descriptor']))
-
-    mmod_tester = MModTester(filename=args.training,thresh_match=0.95,skip_x=8,skip_y=8)
+    mmod_tester = MModTester(thresh_match=0.95,skip_x=8,skip_y=8, model_documents=model_documents)
     
     # Connect the detector to the source
     for key in source.outputs.iterkeys():
-        if key in detector.inputs.keys():
-            plasm.connect(source[key] >> detector[key])
-    plasm.connect(loader['templates', 'objects', 'id_correspondences', 'do_update'] >>
-                        detector['templates', 'objects', 'id_correspondences', 'do_update'])
+        if key in mmod_tester.inputs.keys():
+            plasm.connect(source[key] >> mmod_tester[key])
 
     #visualize raw data
     fps = highgui.FPSDrawer()
@@ -94,7 +75,3 @@ def test_mmod(args):
           )
     sched = ecto.schedulers.Singlethreaded(plasm)
     sched.execute()
-
-if "__main__" == __name__:
-    args = parse_args()
-    test_mmod(args)
